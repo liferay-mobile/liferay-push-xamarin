4ddfb3cab24c9b81e2914fd36e558a89e29b1cc9
Sarai Díaz García <dgarciasarai@gmail.com>
2018-06-12 09:19:40 +0200

    Use binding library to register ios device in Liferay instance

--- Push-iOS/AppDelegate.cs
+++ Push-iOS/AppDelegate.cs
@@ -1,15 +1,17 @@
-﻿using Foundation;
+﻿using System;
+using CoreFoundation;
+using Foundation;
+using LiferayPush;
 using UIKit;
+using UserNotifications;
 
 namespace PushiOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
     [Register("AppDelegate")]
-    public class AppDelegate : UIApplicationDelegate
+    public class AppDelegate : UIApplicationDelegate, IUNUserNotificationCenterDelegate
     {
-        // class-level declarations
-
         public override UIWindow Window
         {
             get;
@@ -18,42 +20,45 @@ namespace PushiOS
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
-            // Override point for customization after application launch.
-            // If not required for your application you can safely delete this method
+            var Center = UNUserNotificationCenter.Current;
+            Center.Delegate = this;
+
+            Center.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound, (approved, error) =>
+            {
+                if (approved)
+                {
+                    DispatchQueue.MainQueue.DispatchAsync(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                    Console.WriteLine("Authorization approved: " + approved);
+                }
+                else
+                {
+                    Console.WriteLine("Error: " + error);
+                }
+            });
 
             return true;
         }
 
-        public override void OnResignActivation(UIApplication application)
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
         {
-            // Invoked when the application is about to move from active to inactive state.
-            // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
-            // or when the user quits the application and it begins the transition to the background state.
-            // Games should use this method to pause the game.
+            LRBasicAuthentication BasicAuthentication = new LRBasicAuthentication("test@liferay.com", "test");
+            LRSession Session = new LRSession("http://192.168.50.124:8080/", BasicAuthentication);
+            LRPush.WithSession(Session)
+                  .WithPortalVersion(70)
+                  .OnSuccess((obj) => Console.WriteLine("success"))
+                  .OnFailure((err) => Console.WriteLine($"failed: {err}"))
+                  .RegisterDeviceTokenData(deviceToken);
         }
 
-        public override void DidEnterBackground(UIApplication application)
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
         {
-            // Use this method to release shared resources, save user data, invalidate timers and store the application state.
-            // If your application supports background exection this method is called instead of WillTerminate when the user quits.
-        }
-
-        public override void WillEnterForeground(UIApplication application)
-        {
-            // Called as part of the transiton from background to active state.
-            // Here you can undo many of the changes made on entering the background.
-        }
-
-        public override void OnActivated(UIApplication application)
-        {
-            // Restart any tasks that were paused (or not yet started) while the application was inactive. 
-            // If the application was previously in the background, optionally refresh the user interface.
-        }
-
-        public override void WillTerminate(UIApplication application)
-        {
-            // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
+            UIAlertView Alert = new UIAlertView()
+            {
+                Title = "Error registering push notifications",
+                Message = error.LocalizedDescription
+            };
+            Alert.AddButton("OK");
+            Alert.Show();
         }
     }
 }
-
