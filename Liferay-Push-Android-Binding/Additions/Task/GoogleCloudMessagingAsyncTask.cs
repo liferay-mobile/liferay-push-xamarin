dd6a0f59b00688028233c4599e43307207aa356f
Sarai Díaz García <dgarciasarai@gmail.com>
2018-06-12 11:29:32 +0200

    Add aar file to generate binding for push android and fix compiling errors

---
+++ Liferay-Push-Android-Binding/Additions/Task/GoogleCloudMessagingAsyncTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.Liferay.Mobile.Push.Task
+{
+    public partial class GoogleCloudMessagingAsyncTask
+    {
+        protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] p0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
