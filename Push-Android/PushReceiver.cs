4454fe1c3f62005817c7e49f511f83ff31293241
Sarai Díaz García <dgarciasarai@gmail.com>
2018-06-18 12:07:29 +0200

    Create PushReceiver and PushService classes in android sample

---
+++ Push-Android/PushReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Com.Liferay.Mobile.Push;
+
+namespace PushAndroid
+{
+    [BroadcastReceiver(Permission = "com.google.android.c2dm.permission.SEND")]
+    [IntentFilter(new String[]{"com.google.android.c2dm.intent.RECEIVE"})]
+    public class PushReceiver : PushNotificationsReceiver
+    {
+        public PushReceiver() { }
+
+        public override string ServiceClassName => Java.Lang.Class.FromType(typeof(PushService)).Name;
+    }
+}
---
+++ Push-Android/PushService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.App;
+using Org.Json;
+
+namespace PushAndroid
+{
+    [Service]
+	public class PushService : PushNotificationsService
+    {
+        public PushService() { }
+
+        public override void OnPushNotification(JSONObject json)
+        {
+            Console.WriteLine(json);
+        }
+    }
+}
