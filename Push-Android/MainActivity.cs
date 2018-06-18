00fbd511fbb8e3c624c16d809046ab94d167fee4
Sarai Díaz García <dgarciasarai@gmail.com>
2018-06-18 12:08:19 +0200

    Register android device to receive push notifications

--- Push-Android/MainActivity.cs
+++ Push-Android/MainActivity.cs
@@ -1,26 +1,37 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+using Android.App;
 using Android.OS;
+using Com.Liferay.Mobile.Android.Auth.Basic;
+using Com.Liferay.Mobile.Android.Service;
+using Com.Liferay.Mobile.Push;
+using Org.Json;
 
 namespace PushAndroid
 {
-    [Activity(Label = "Push-Android", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "PushAndroid", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity, Push.IOnSuccess, Push.IOnFailure
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            var Session = new SessionImpl("http://10.0.2.2:8080", new BasicAuthentication("test@liferay.com", "test"));
+            Push.With(Session)
+                .WithPortalVersion(70)
+                .OnSuccess(this)
+                .OnFailure(this)
+                .Register(this, "393909434085");
+        }
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        public void OnSuccess(JSONObject p0)
+        {
+            Console.WriteLine($"Device registered succesfully: {p0}");
+        }
+
+        public void OnFailure(Java.Lang.Exception p0)
+        {
+            Console.WriteLine($"Device register failed: {p0.Message}");
         }
     }
 }
